38075b754f4e6b38954518975945cd9bbd9fc9ae
Nuraib <nuraibaa@code.edu.az>
2023-10-16 19:42:19 +0400

    adding method, when i click the return button,adding .search-form-input-row class

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,40 @@ window.onload = function(){
         mobile_menu.classList.toggle('is-active');
     })
 }
+
+const searchReturn = document.querySelector('#return');
+const searchOneway = document.querySelector('#one-way')
+const searchFormInput = document.querySelector('.search-form-input-row');
+const searchForm = document.querySelector('.search-form');
+
+
+searchReturn.addEventListener("click", function(){
+    searchForm.innerHTML += `
+    <div class="search-form-input-row">
+    <div class="search-form-input-row-dpt">
+        <div class="input">
+            <div class="label-container"><p class="label-container-text">Departure</p></div>
+            <div class="input-text">
+                <input class="search-input-text" type="text" placeholder="Singapure (SIN)">
+            </div>
+        </div>
+    </div>
+    <div class="search-form-input-row-arr">
+        <div class="input">
+            <div class="label-container"><p class="label-container-text">Arrival</p></div>
+            <div class="input-text">
+                <input class="search-input-text" type="text" placeholder="LosAngeles (LA)">
+            </div>
+        </div>
+    </div>
+    <div class="search-form-input-row-date">
+        <div class="input">
+            <div class="label-container"><p class="label-container-text">Date</p></div>
+            <div class="input-text">
+                <input class="search-input-text" type="date">
+            </div>
+        </div>
+    </div>
+</div>
+    `;
+})
