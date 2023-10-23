e0576720f2210444f1819098e98bd776977ba407
Nuraib <nuraibaa@code.edu.az>
2023-10-23 20:49:08 +0400

    cards changing dynamically

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,9 +1,12 @@
-let cityContainer = document.querySelector(".cards-container")
-let hotels = document.getElementsByClassName("hotel-cards");
+let cityContainer = document.querySelector(".cards-container");
+let hotels = document.querySelector(".hotel-cards");
+let recHotels = document.querySelector(".rec-cards-container");
+
 
 document.addEventListener("DOMContentLoaded", function(){
     ListCities();
     ListHotels();
+    RecommendedHotels();
 })
 
 async function ListCities() {
@@ -20,13 +23,13 @@ async function ListCities() {
         data.forEach(city => {
             cityContainer.innerHTML += `
             <div class="col">
-                    <div class="city">
+                    <div class="city" style="background: url(${city.image})">
                         <div class="destiantion-details"><p class="destiantion-details-text"></p></div>
                         <div class="city-details">
                             <p class="city-details-text">${city.destinationName}</p>
                             <div class=ct-details">
                                 <p class="ct-details-first">FROM</p>
-                                <p class="ct-details-second">$699</p>
+                                <p class="ct-details-second">$${city.destinationPrice}</p>
                             </div>
                         </div>
                     </div>
@@ -42,24 +45,22 @@ async function ListHotels() {
     let res = await fetch("http://localhost:4000/hotels/popular");
     let data = await res.json();
 
-    console.log(data)
-
     if (!res.ok) {
         throw new Error("There isn't cities")
     }
 
     if (Array.isArray(data) && data) {
         hotels.innerHTML = "";
-        data.forEach(city => {
+        data.forEach(hotel => {
             hotels.innerHTML += `
             <div class="hotel-col">
                     <div class="hotel-card">
-                        <img src="./assets/images/Matterhorn Suites Image.png" alt="">
+                        <img src="${hotel.img}" alt="">
                         <div class="hotel-details">
                             <div class="hotel-details-row">
-                                <p class="hotel-description">Deluxe King Room</p>
-                                <p class="hotel-name">Lake Louise Inn</p>
-                                <p class="hotel-price">$244/night</p>
+                                <p class="hotel-description">${hotel.hotelDescription}</p>
+                                <p class="hotel-name">${hotel.hotelName}</p>
+                                <p class="hotel-price">$${hotel.hotelPrice}</p>
                             </div>
                             <div class="video-button">
                                 <i class="fa-regular fa-circle-play video-play"></i>
@@ -70,17 +71,46 @@ async function ListHotels() {
 
                             <div class="hotel-star">
                                 <i class="fa-solid fa-star star"></i>
-                                <p class="star-point">4.6</p>
+                                <p class="star-point">${hotel.hotelRating.starPoint}</p>
                             </div>
 
-                            <p class="hotel-review">(63 reviews)</p>
+                            <p class="hotel-review">${hotel.hotelRating.hotelReview}</p>
                         </div>
                         <div class="more-detail-button">
                             <p>more details</p>
                         </div>
                     </div>
                 </div>
-        
+        `
+        });
+    }
+}
+
+async function RecommendedHotels() {
+
+    let res = await fetch("http://localhost:4000/cities");
+    let data = await res.json();
+
+    if (!res.ok) {
+        throw new Error("There isn't cities")
+    }
+
+    if (Array.isArray(data) && data) {
+        recHotels.innerHTML = "";
+        data.forEach(city => {
+            recHotels.innerHTML += `
+            <div class="rec-card">
+                    <div class="rec-card-background" style="background: url(${city.image})"></div>
+                    <div class="holiday-details">
+                        <div class="frame">
+                            <div class="frame-inner">
+                                <p class="frame-inner-first">${city.destinationName}</p>
+                                <p class="frame-inner-second">4D3N</p>
+                            </div>
+                            <p class="frame-text">$${city.destinationPrice}</p>
+                        </div>
+                    </div>
+                </div>
         `
         });
     }
--- server/data/hotels.js
+++ server/data/hotels.js
@@ -1,51 +1,107 @@
-export let DPopularHotels=[
-    {
-        id:1,
-        typeof:"Deluxe",
-        tittle:"Matterhorn Suites",
-        img:"assets\images\Matterhorn Suites Image.png",
-        price:{amount:575, currency:"USD"},
-        review:{
-            star:4.5,
-            count:60
-        },
+export let DPopularHotels=[{
+    id:1,
+    hotelDescription:"Entire bungalow",
+    hotelName:"Matterhorn Suites",
+    hotelPrice:575,
+    hotelRating:{
+        starPoint: 4.9,
+        hotelReview: 60
     },
-    {
-        id:2,
-        typeof:"Deluxe",
-        tittle:"Discovery Shores",
-        img:"assets\images\Discovery Shores Image.png",
-        price:{amount:360, currency:"USD"},
-        review:{
-            star:4.8,
-            count:116
-        },
+    img:"./assets/images/Matterhorn Suites Image.png",
+},
+{
+    id:2,
+    hotelDescription:"2-Story beachfront suite",
+    hotelName:"Discovery Shores",
+    hotelPrice:360,
+    hotelRating:{
+        starPoint: 4.8,
+        hotelReview: 116
     },
-    {
-        id:3,
-        typeof:"Deluxe",
-        tittle:"Arctic Hut",
-        img:"assets\images\Arctic Hut Image.png",
-        price:{amount:420, currency:"USD"},
-        review:{
-            star:4.7,
-            count:78
-        },
-    },{
-        id:4,
-        typeof:"Deluxe",
-        tittle:"Lake Louise Inn",
-        img:"assets\images\Lake Louise Image.png",
-        price:{amount:224, currency:"USD"},
-        review:{
-            star:4.6,
-            count:63
-        },
-    }
+    img:"./assets/images/Discovery Shores Image.png",
+},
+{
+    id:3,
+    hotelDescription:"Single deluxe hut",
+    hotelName:"Arctic Hut",
+    hotelPrice:420,
+    hotelRating:{
+        starPoint: 4.7,
+        hotelReview: 78
+    },
+    img:"./assets/images/Arctic Hut Image.png",
+},
+{
+    id:4,
+    hotelDescription:"Deluxe King Room",
+    hotelName:"Lake Louise Inn",
+    hotelPrice:244,
+    hotelRating:{
+        starPoint: 4.6,
+        hotelReview: 63
+    },
+    img:"./assets/images/Lake Louise Image.png",
+}
 ]
 
-export let DHotel=[
-...DPopularHotels,
-...DPopularHotels,
-...DPopularHotels,
-]
+export let DHotel = [
+    ...DPopularHotels,
+    ...DPopularHotels,
+    ...DPopularHotels,
+    ...DPopularHotels,
+
+{
+    id:5,
+    hotelDescription:"Deluxe King Room",
+    hotelName:"Lake Louise Inn",
+    hotelPrice:244,
+    hotelRating:{
+        starPoint: 4.6,
+        hotelReview: 63
+    },
+    img:"./Hotels/assets/images/Results Image.png",
+},
+{
+    id:7,
+    hotelDescription:"Deluxe King Room",
+    hotelName:"Lake Louise Inn",
+    hotelPrice:244,
+    hotelRating:{
+        starPoint: 4.6,
+        hotelReview: 63
+    },
+    img:"./Hotels/assets/images/Lake Louise Image.png",
+},
+{
+    id:8,
+    hotelDescription:"Deluxe King Room",
+    hotelName:"Lake Louise Inn",
+    hotelPrice:244,
+    hotelRating:{
+        starPoint: 4.6,
+        hotelReview: 63
+    },
+    img:"./Hotels/assets/images/Lake Louise Image.png",
+},
+{
+    id:9,
+    hotelDescription:"Deluxe King Room",
+    hotelName:"Lake Louise Inn",
+    hotelPrice:244,
+    hotelRating:{
+        starPoint: 4.6,
+        hotelReview: 63
+    },
+    img:"./Hotels/assets/images/Lake Louise Image.png",
+},
+{
+    id:10,
+    hotelDescription:"Deluxe King Room",
+    hotelName:"Lake Louise Inn",
+    hotelPrice:244,
+    hotelRating:{
+        starPoint: 4.6,
+        hotelReview: 63
+    },
+    image:"./Hotels/assets/images/Lake Louise Image.png",
+}]
